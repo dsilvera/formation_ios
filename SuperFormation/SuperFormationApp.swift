dc8b6f54d9a8a12b961bb8d8ba1c8b764e5430e0
David Silvera <dav.silvera@gmail.com>
2022-11-17 14:31:14 +0100

    Initial Commit

---
+++ SuperFormation/SuperFormationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperFormationApp.swift
+//  SuperFormation
+//
+//  Created by David on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperFormationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
